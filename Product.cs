b8aed6479f646c04d9e3eacba2e3486f6ad5be25
Hieu Phan <64969360+Pogchop@users.noreply.github.com>
2021-01-21 20:26:56 +0700

    Add files via upload

---
+++ Product.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EFcore.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string name { get; set; }
+    }
+}
